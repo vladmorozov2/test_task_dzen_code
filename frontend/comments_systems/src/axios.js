57708a9a591b1357507fb383d00ef19135ebb9bb
Morozov Vladyslav <vladmorozov@knu.ua>
2025-07-03 00:16:44 +0300

    changed ip

--- frontend/comments_systems/src/axios.js
+++ frontend/comments_systems/src/axios.js
@@ -2,7 +2,10 @@
 import axios from 'axios'
 
 const instance = axios.create({
+  
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',     
 })
+console.log('API Base URL:', import.meta.env.VITE_API_BASE_URL)
+
 
 export default instance
